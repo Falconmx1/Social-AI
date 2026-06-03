5eeb61dee773fe237f16328e61f552c5d3ebca6e
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-06-03 03:03:33 -0600

    Create pretext_gen.py

---
+++ modules/pretext_gen.py
@@ -0,0 +1,5 @@
+def generate_pretext(data):
+    # Usarías una IA real (ollama, openai, etc.)
+    print("[*] Generando pretexto con IA...")
+    pretexto = f"Hola, soy del departamento de IT. Necesito verificar tu usuario: {data['emails'][0]}"
+    return pretexto
